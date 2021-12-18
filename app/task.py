d23587d9942efa1631f46729a53c151b8b0aa915
iCaran <91419527+iCaran@users.noreply.github.com>
2021-12-18 19:54:39 +0530

    Update task.py

--- app/task.py
+++ app/task.py
@@ -1,175 +1,226 @@
-import sys
+const fs = require("fs");
+const { execSync } = require("child_process");
 
-def help():
-    sa = """Usage :-
+let deleteFile = (path) => {
+  try {
+    fs.unlinkSync(path);
+  } catch (err) {}
+};
+
+beforeEach(() => {
+  deleteFile(`${__dirname}/task.txt`);
+  deleteFile(`${__dirname}/completed.txt`);
+});
+
+let tasksTxtCli = (...args) => [`${__dirname}/task`, ...args].join(" ");
+
+let usage = `Usage :-
 $ ./task add 2 hello world    # Add a new item with priority 2 and text "hello world" to the list
 $ ./task ls                   # Show incomplete priority list items sorted by priority in ascending order
 $ ./task del INDEX            # Delete the incomplete item with the given index
 $ ./task done INDEX           # Mark the incomplete item with the given index as complete
 $ ./task help                 # Show usage
-$ ./task report               # Statistics"""
-    sys.stdout.buffer.write(sa.encode('utf8'))
-    
-def add(n,s):
-    try:
-        with open('../tasks/task.txt', 'r') as f:
-            st=s+" ["+n+"]\n"
-            lines=f.readlines()
-            if len(lines)==0:
-                with open('../tasks/task.txt', 'w') as g:
-                    g.write("".join(st))
-                    g.close()
-                    s = '"'+s+'"'
-                    print(f"Added task: {s} with priority {n}")
-            else:
-                if st not in lines:
-                    pr=[]
-                    for line in lines:
-                        i=''.join(lines).rindex(line)
-                        line=line.strip('\n')
-                        beg=line.rfind("[")
-                        #if st==line[:beg]: break
-                        p=line[beg+1:-1]
-                        pr.append(p)
-                        
-                    for pri in pr:
-                        if(n<pri):
-                            lines.insert(pr.index(pri),st)
-                            break
-                        elif pr.index(pri)==len(pr)-1:
-                            lines.append(st)
-                    
-                    with open('../tasks/task.txt', 'w') as f:
-                        f.write("".join(lines))
-                        f.close()
-                        s = '"'+s+'"'
-                        print(f"Added task: {s} with priority {n}")
-                else:
-                    print('''Added task: "'''+s+'''"''')
-    except:
-        print("Could not add item. Try help.")
-    
-def ls():
-    
-    try:
-  
-        nec()
-        l = 1
-        k = l
-        if len(d)>0:
-            for i in d:
-                sys.stdout.buffer.write(f"{l}. {d[l]}".encode('utf8'))
-                sys.stdout.buffer.write("\n".encode('utf8'))
-                l = l+1
-        else: sys.stdout.buffer.write("There are no pending tasks!".encode('utf8'))
-  
-    except Exception as e:
-        raise e
-        
-def done(no):
-    try:
-  
-        nec()
-        no = int(no)
-        f = open('../tasks/completed.txt', 'a')
-        st = d[no]
-          
-        f.write(st)
-        f.write("\n")
-        f.close()
-        print(f"Marked item as done.")
-          
-        with open("../tasks/task.txt", "r+") as f:
-            lines = f.readlines()
-            f.seek(0)
-              
-            for i in lines:
-                if i.strip('\n') != d[no]:
-                    f.write(i)
-            f.truncate()
-    except:
-        print(f"Error: task #{no} does not exist.")
-        #pass
-        
-def report():
-    nec()
-    try:
-        nf = open('../tasks/completed.txt', 'r')
-        c = 1
-          
-        for line in nf:
-            line = line.strip('\n')
-            don.update({c: line})
-            c = c+1
-        print(
-            f'Pending : {len(d)} \n\nCompleted : {len(don)}')
-      
-    except:
-        print(
-            f'Pending : {len(d)} \n\nCompleted : {len(don)}')
-            
-def deL(no):
-    try:
-        no = int(no)
-        nec()
-  
-        with open("../tasks/task.txt", "r+") as f:
-            lines = f.readlines()
-            f.seek(0)
-              
-            for i in lines:
-                if i.strip('\n') != d[no]:
-                    f.write(i)
-            f.truncate()
-        print(f"Deleted task #{no}")
-  
-    except Exception as e:
-        
-        print(f"Error: task #{no} does not exist. Nothing deleted.")
-        
-def nec():
-  
-    try:
-        f = open('../tasks/task.txt', 'r')
-        c = 1
-        #if len(f)>0:
-        for line in f:
-            line = line.strip('\n')
-            d.update({c: line})
-            c = c+1
-            #print(line)
-        #else:
-        #    sys.stdout.buffer.write("There are no pending tasks!".encode('utf8'))
-    except:
-        sys.stdout.buffer.write("There are no pending tasks!".encode('utf8'))
-  
-  
-if __name__ == '__main__':
-    try:
-        d = {}
-        don = {}
-        args = sys.argv
-        
-        if (len(args[1:])==0):
-            help()
-        if(args[1] == 'del'):
-            args[1] = 'deL'
-              
-        if(args[1] == 'add' and len(args[2:]) == 0 and len(args[3:]) == 0):
-            sys.stdout.buffer.write(
-                "Error: Missing tasks string. Nothing added!".encode('utf8'))
-  
-        elif(args[1] == 'done' and len(args[2:]) == 0):
-            sys.stdout.buffer.write(
-                "Error: Missing NUMBER for marking task as done.".encode('utf8'))
-  
-        elif(args[1] == 'deL' and len(args[2:]) == 0):
-            sys.stdout.buffer.write(
-                "Error: Missing NUMBER for deleting task.".encode('utf8'))
-        else:
-            globals()[args[1]](*args[2:])
-  
-    except Exception as e:
-        #if (len(args[1:])!=0):
-        #    help()
-        pass
+$ ./task report               # Statistics`;
+
+test("prints help when no additional args are provided", () => {
+  let received = execSync(tasksTxtCli()).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(usage));
+});
+
+test("prints help", () => {
+  let received = execSync(tasksTxtCli("help")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(usage));
+});
+
+test("add a single tasks", () => {
+  let expected = 'Added task: "the thing i need to do" with priority 1';
+  let received = execSync(
+    tasksTxtCli("add", '1 "the thing i need to do"')
+  ).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("show error message when add is not followed by a tasks", () => {
+  let expected = "Error: Missing tasks string. Nothing added!";
+  let received = execSync(tasksTxtCli("add")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("add multiple tasks", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+
+  tasks.forEach((tasks, i) => {
+    let expected = `Added task: "${tasks}"`;
+    let received = execSync(tasksTxtCli("add", `${i + 1} "${tasks}"`)).toString(
+      "utf8"
+    );
+
+    expect(received).toEqual(expect.stringContaining(expected));
+  });
+});
+
+test("list tasks in order of priority", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) =>
+    execSync(tasksTxtCli("add", `${i + 1} "${tasks}"`))
+  );
+
+  let expected = `1. the thing i need to do [1]
+2. water the plants [2]
+3. find needle in the haystack [3]
+`;
+  let received = execSync(tasksTxtCli("ls")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("list when there are no remaining tasks", () => {
+  let expected = `There are no pending tasks!`;
+  let received = execSync(tasksTxtCli("ls")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("delete a task", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((task, i) =>
+    execSync(tasksTxtCli("add", `${i + 1} "${task}"`))
+  );
+
+  let expected = "Deleted task #2";
+  let received = execSync(tasksTxtCli("del", "2")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("delete tasks numbered 3, 2 & 1", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) =>
+    execSync(tasksTxtCli("add", `${i + 1} "${tasks}"`))
+  );
+
+  [3, 2, 1].forEach((n) => {
+    let expected = `Deleted task #${n}`;
+    let received = execSync(tasksTxtCli("del", n.toString())).toString("utf8");
+
+    expect(received).toEqual(expect.stringContaining(expected));
+  });
+});
+
+test("delete first task 3 times", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) => execSync(tasksTxtCli("add", `${i} "${tasks}"`)));
+
+  [1, 1, 1].forEach((n) => {
+    let expected = `Deleted task #${n}`;
+    let received = execSync(tasksTxtCli("del", n.toString())).toString("utf8");
+
+    expect(received).toEqual(expect.stringContaining(expected));
+  });
+});
+
+test("delete non-existent tasks", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) => execSync(tasksTxtCli("add", `${i} "${tasks}"`)));
+
+  [0, 4, 5].forEach((n) => {
+    let expected = `Error: task with index #${n} does not exist. Nothing deleted.`;
+    let received = execSync(tasksTxtCli("del", n.toString())).toString("utf8");
+
+    expect(received).toEqual(expect.stringContaining(expected));
+  });
+});
+
+test("delete does not have enough arguments", () => {
+  let expected = "Error: Missing NUMBER for deleting tasks.";
+  let received = execSync(tasksTxtCli("del")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("mark a tasks as done", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) => execSync(tasksTxtCli("add", `${i} "${tasks}"`)));
+
+  let expected = "Marked item as done.";
+  let received = execSync(tasksTxtCli("done", "2")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("mark as done a tasks which does not exist", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) => execSync(tasksTxtCli("add", `${i} "${tasks}"`)));
+
+  let expected = "Error: no incomplete item with index #0 exists.";
+  let received = execSync(tasksTxtCli("done", "0")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("mark as done without providing a tasks number", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) => execSync(tasksTxtCli("add", `${i} "${tasks}"`)));
+
+  let expected = "Error: Missing NUMBER for marking tasks as done.";
+  let received = execSync(tasksTxtCli("done")).toString("utf8");
+
+  expect(received).toEqual(expect.stringContaining(expected));
+});
+
+test("report pending & completed tasks", () => {
+  let tasks = [
+    "the thing i need to do",
+    "water the plants",
+    "find needle in the haystack",
+  ];
+  tasks.forEach((tasks, i) => execSync(tasksTxtCli("add", `${i} "${tasks}"`)));
+
+  execSync(tasksTxtCli("done", "1"));
+  execSync(tasksTxtCli("done", "2"));
+
+  let date = new Date();
+  let expected = `Pending : 1\n1. water the plants [1]\n\nCompleted : 2\n1. the thing i need to do\n2. find needle in the haystack\n`;
+  let received = execSync(tasksTxtCli("report")).toString("utf8");
+  expect(received).toEqual(expect.stringContaining(expected));
+});
